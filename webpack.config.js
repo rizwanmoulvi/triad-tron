5ada00c1d5eb8c5a331cb5590291603144d2bd63
Rizwan <rizwan.m.1610@gmail.com>
2024-05-08 02:35:56 +0530

    Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ module.exports = {
     //...
     resolve: {
       fallback: {
-        "crypto": false
-      }
-    }
-  };
+        crypto: require.resolve("crypto-browserify"),
+        querystring: require.resolve("querystring-es3"),
+        buffer: false,
+        stream: false,
+    },
+},
+}
